1fc28ed3ab1ef99258e011e47e7d7e67959b6f7e
Nicholas O'Kelley <okelleynp@gmail.com>
2022-02-08 23:09:26 -0500

    feat: Comment out which key mapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -39,7 +39,7 @@ telescope.setup {
                 -- map actions.which_key to <C-h> (default: <C-/>)
                 -- actions.which_key shows the mappings for your picker,
                 -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-                ["<C-h>"] = "which_key",
+                --["<C-h>"] = "which_key",
             },
         },
     },
