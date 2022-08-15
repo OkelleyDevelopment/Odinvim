7e5258d075548f91851e74cbc40e4e727643ce24
Nicholas O'Kelley <okelleynp@gmail.com>
2022-08-14 20:40:58 -0400

    feat: Corrects previous adjustments a bit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -62,7 +62,7 @@ return packer.startup(function(use)
 
     ----- Themes -----
     use "rebelot/kanagawa.nvim"
-    
+
     ----- Syntax Highlight ----
     use "OkelleyDevelopment/vim-solidity"
     use "aklt/plantuml-syntax"
@@ -86,18 +86,6 @@ return packer.startup(function(use)
                 return
             end
             gitsigns.setup()
-        end,
-    }
-
-    use { -- Rust Tools
-        "simrat39/rust-tools.nvim",
-    }
-    use {
-        "saecki/crates.nvim",
-        tag = "v0.2.1",
-        requires = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("crates").setup()
         end,
     }
 
@@ -148,6 +136,15 @@ return packer.startup(function(use)
     --  "nvim-treesitter/nvim-treesitter",
     --run = ":TSUpdate",
     --}
+
+    use {
+        "saecki/crates.nvim",
+        tag = "v0.2.1",
+        requires = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("crates").setup()
+        end,
+    }
 
     --------- Language Server Plugins ---------
     --
