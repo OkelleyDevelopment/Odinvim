d565b57bd76ff4d8603a5324857b62ad4e2bcc38
Nicholas O'Kelley <okelleynp@gmail.com>
2022-10-21 20:33:20 -0400

    feat: disable netrw

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -12,6 +12,10 @@ local statusline = require "core.statusline"
 
 -- Set leader key
 vim.g.mapleader = " " -- Map leader key
+-- Disables netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 
 -------------------------- General Settings ---------------------------
 
