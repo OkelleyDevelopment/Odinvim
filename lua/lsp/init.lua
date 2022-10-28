b97965bc3b4af664ea19c45ff3c46ee6483adc07
Nicholas O'Kelley <okelleynp@gmail.com>
2022-10-28 18:17:42 -0400

    fix: add lua server config back

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -29,3 +29,10 @@ lspconfig["tsserver"].setup({
     on_attach = core.on_attach,
     capabilities = core.capabilities,
 })
+
+[[--
+lspconfig["sumneko_lua"].setup({
+    on_attach = core.on_attach,
+    capabilities = core.capabilities,
+})
+--]]
